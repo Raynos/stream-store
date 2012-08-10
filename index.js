1ef9333235331322774019eaf47cb82cd0e000a9
Raynos <raynos2@gmail.com>
2012-08-10 15:35:13 -0700

    Fully ES6 Map compliant

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var MemoryStore = require("memory-store")
     , through = require("through")
+    , globalContext = new Function('return this')()
 
 module.exports = StreamStore
 
@@ -8,12 +9,17 @@ function StreamStore(Stream) {
     Stream = Stream || through
 
     return {
-        get: get
-        , set: set
+        set: set
+        , get: get
+        , has: has
         , delete: $delete
+        , keys: keys
+        , values: values
+        , iterate: iterate
+        , toArray: toArray
     }
 
-    function get(name, callback) {
+    function get(name) {
         var stream = store[name]
 
         if (!stream) {
@@ -23,11 +29,52 @@ function StreamStore(Stream) {
         return stream
     }
 
+    function has(name) {
+        return name in store
+    }
+
+    function keys() {
+        return Object.keys(store)
+    }
+
+    function values() {
+        return Object.keys(store).map(toValue, store)
+    }
+
+    function toArray() {
+        return Object.keys(store).map(toKeyValue, store)
+    }
+
+    function iterate(callback, context) {
+        var keys = Object.keys(store)
+            , length = keys.length
+
+        context = context || globalContext
+
+        for (var i = 0; i < length; i++) {
+            var key = keys[i]
+                , value = store[key]
+
+            callback.call(context, value, key, store)
+        }
+    }
+
     function set(name, stream) {
         store[name] = stream
     }
 
     function $delete(name) {
         return delete store[name]
+    }
+}
+
+function toValue(key) {
+    return this[key]
+}
+
+function toKeyValue(key) {
+    return {
+        key: key
+        , value: this[key]
     }
 }
